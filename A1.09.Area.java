cab63a615cfc60672708fc24596dbf530f074f06
Sachin-Chouhan201 <75372486+Sachin-Chouhan201@users.noreply.github.com>
2021-09-20 13:04:43 +0530

    Create A1.09.Area.java

---
+++ A1.09.Area.java
@@ -0,0 +1,11 @@
+// Java program to find area of circle
+	
+class Area
+{
+	public static void main(String[] args)
+	{
+    double PI = Math.PI;
+    double area=PI*Math.pow(5, 2);//here 5 is radius
+		System.out.println("Area is " + area);
+	}
+}
